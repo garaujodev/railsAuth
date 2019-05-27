4427e56683f34ca8b57d11e7be2bb689f5141586
Gustavo Araujo <garaujodev@gmail.com>
2019-05-26 21:48:27 -0300

    :sparkles: Add User Update endpoint

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,26 +1,42 @@
 class AuthController < ApplicationController
   def show 
-    usr = User.find_by_id(params[:id]) 
-    if usr.nil?
-      return render json: {result: false, message: 'Record not found!'}, status: 404
+    user = User.find_by_id(params[:id]) 
+    if user.nil?
+      return render json: {success: false, message: 'User not found!'}, status: :not_found
     end
-    render json: usr
+    
+    render json: user
   end
 
   def list
-    usrs = User.all
-    render json: usrs
+    users = User.all
+
+    render json: {success: true, users: users}, status: :ok
   end
 
   def update
+    user = User.find_by_id(params[:id])
+    if user.nil?
+      return render json: {success: false, message: 'User not found!'}, status: :not_found
+    end
 
+    if user.update_attributes(user_params)
+      render json: {success: true, user: user}, status: :ok
+    else
+      render json: {success: false, user: user}, status: :unprocessable_entity
+    end
   end
 
   def delete
-    usr = User.find(params[:id])
-    usr.destroy
-    render json: 'sucess'
+    user = User.find(params[:id])
+    user.destroy
 
+    render json: {success: true, message: 'User deleted succesfuly!'}, status: :ok
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :role)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'user/list', to: 'auth#list'
   get 'user/:id', to: 'auth#show'
+  put 'user/update/:id', to: 'auth#update'
   delete 'user/:id', to: 'auth#delete'
+
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
