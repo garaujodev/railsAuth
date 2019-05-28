7b6aa2862a423a80ff691c89e6e1beda6b2d82a9
Gustavo Araujo <garaujodev@gmail.com>
2019-05-27 23:55:48 -0300

    :hammer: Add refactoring to auth_controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,35 +1,41 @@
 class AuthController < ApplicationController
+  before_action :authenticate_user!
+  
   def show 
-    user = User.find_by_id(params[:id]) 
-    if user.nil?
+    @user = User.find_by_id(params[:id]) 
+    if @user.nil?
       return render json: {success: false, message: 'User not found!'}, status: :not_found
     end
     
-    render json: user
+    render json: {success: true, user: @user}, status: :ok
   end
 
   def list
-    users = User.all
+    @users = User.all
 
-    render json: {success: true, users: users}, status: :ok
+    render json: {success: true, users: @users}, status: :ok
   end
 
   def update
-    user = User.find_by_id(params[:id])
-    if user.nil?
+    @user = User.find_by_id(params[:id])
+    if @user.nil?
       return render json: {success: false, message: 'User not found!'}, status: :not_found
     end
 
-    if user.update_attributes(user_params)
-      render json: {success: true, user: user}, status: :ok
+    if @user.update_attributes(user_params)
+      render json: {success: true, user: @user}, status: :ok
     else
-      render json: {success: false, user: user}, status: :unprocessable_entity
+      render json: {success: false, user: @user}, status: :unprocessable_entity
     end
   end
 
   def delete
-    user = User.find(params[:id])
-    user.destroy
+    @user = User.find_by_id(params[:id])
+    if @user.nil?
+      return render json: {success: false, message: 'User not found!'}, status: :not_found
+    end
+
+    @user.destroy
 
     render json: {success: true, message: 'User deleted succesfuly!'}, status: :ok
   end
@@ -38,6 +44,5 @@ class AuthController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :role)
   end
-
-
+  
 end
