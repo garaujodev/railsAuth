a7a5f62632bb8522eca769993aa2fe5145b59474
Gustavo Araujo <garaujodev@gmail.com>
2019-05-30 09:30:09 -0300

    :heavy_check_mark: Add make test pass: controller auth_controller

--- spec/controllers/auth_controller_spec.rb
+++ spec/controllers/auth_controller_spec.rb
@@ -3,9 +3,28 @@ require 'rails_helper'
 RSpec.describe AuthController, type: :controller do
 
   describe "GET #get" do
-    it "returns http success" do
+    it "returns http unauthorized" do
       get :get
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+
+    describe "GET #show" do
+    it "returns http unauthorized" do
+      get :get
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+    describe "GET #update" do
+    it "returns http unauthorized" do
+      get :get
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+    describe "GET #delete" do
+    it "returns http unauthorized" do
+      get :get
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 
